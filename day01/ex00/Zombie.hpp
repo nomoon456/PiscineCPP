cc47c103c8bf04a5bddadd0d4d926356300349a8
nomoon456@gmail.com <nomoon456@gmail.com>
2021-09-20 08:46:58 +0200

    cheh

---
+++ day01/ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+# include <iostream>
+# include <iomanip>
+
+class Zombie {
+    public:
+            Zombie(std::string name);
+            ~Zombie(void);
+            void    Announce(void);
+    private:
+
+};
+
+#endif
